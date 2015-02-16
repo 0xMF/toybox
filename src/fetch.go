deb2cb4cffe9f13ab0e9e325094cf9dd13c27f8c
Mark Fernandes <0xmf@outlook.com>
2015-02-15 20:50:37 -0500

    first pass at parsing ADN json (broken)

--- src/fetch.go
+++ src/fetch.go
@@ -1,23 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
 	"net/http"
-  "log"
 )
 
 func main() {
+	type Marker struct {
+		id         string
+		name       string
+		Percentage int
+		Updated_at string
+		Version    string
+	}
+	type Meta struct {
+		code   int
+		marker Marker
+		max_id string
+		min_id string
+		more   bool
+	}
+	type Response struct {
+		data string
+		meta Meta
+	}
+
 	adn_global := "https://api.app.net/posts/stream/global"
 
 	res, err := http.Get(adn_global)
 	if err != nil {
 		log.Fatal(err)
 	}
-  global, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
+	//global, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	dec := json.NewDecoder(io.Reader(res.Body))
+	for {
+		var r Response
+		if err := dec.Decode(&r); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+    fmt.Printf("%+v\n", r)
 	}
-	fmt.Printf("%s",global)
 }
