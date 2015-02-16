073b47d751f74066c44994e9714aa10dcc68a1de
Mark Fernandes <0xmf@outlook.com>
2015-02-16 14:29:46 -0500

    Parses ADN feed correctly

--- src/fetch.go
+++ src/fetch.go
@@ -3,83 +3,112 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
+	type HashTag struct {
+		Len  int `json:"len"`
+		Name string
+		Pos  int
+	}
+	type Link struct {
+		Url  string
+		Text string
+		Pos  int
+		Len  int `json:"len"`
+	}
+	type Mention struct{}
+	type Entity struct {
+		Mentions []Mention
+		Hashtags []HashTag
+		Links    []Link
+	}
+	type Source struct {
+		Link      string
+		Name      string
+		Client_Id string
+	}
+	type Image struct {
+		Url        string
+		Width      int
+		Is_Defualt bool
+		Height     int
+	}
+	type Description struct {
+		Text     string
+		Html     string
+		Entities Entity
+	}
+	type Count struct {
+		Following int
+		Posts     int
+		Stars     int
+	}
+	type User struct {
+		Username         string
+		Avatar_Image     Image
+		User_Description Description `json:"description"`
+		Locale           string
+		Created_At       string
+		Canonical_Url    string
+		Cover_Image      Image
+		Timezone         string
+		Counts           Count
+		User_Type        string `json:"type"`
+		Id               string
+		Name             string
+	}
+	type Location struct {
+		Latitude  float32
+		Longitude float32
+	}
+	type Annotation struct {
+		Annotation_type string `json:"type"`
+		Value           Location
+	}
+
+	type Post struct {
+		Created_At    string
+		Num_Stars     int
+		Num_Replies   int
+		Source        Source
+		Text          string
+		Num_Reposts   int
+		Id            string
+		Canonical_Url string
+		Entities      Entity
+		Html          string
+		Machine_Only  bool
+		Post_User     User `json:"user"`
+		Thread_Id     string
+		Pagination_Id string
+		You_Reposted  bool
+		You_Starred   bool
+		Annotations   []Annotation
+		Reposters     []User
+		Starred_By    []User
+	}
+
 	type Marker struct {
-		id         string
-		name       string
+		Id         string
+		Name       string
 		Percentage int
 		Updated_at string
 		Version    string
 	}
 	type Meta struct {
-		code   int
-		marker Marker
-		max_id string
-		min_id string
-		more   bool
+		Min_Id      string `jsun: "min_id"`
+		Code        int    `json: "code"`
+		Meta_Marker Marker
+		Max_Id      string `json: "max_id"`
+		More        bool   `json: "more"`
 	}
 	type Response struct {
-		data string
-		meta Meta
-	}
-
-	type HashTag struct {
-		len  int
-		name string
-		pos  int
-	}
-	type Link struct {
-		len  int
-		pos  int
-		text string
-		url  string
-	}
-	type Mention struct{}
-	type Entity struct {
-		hashtags []HashTag
-		links    []Link
-		mentions []Mention
-	}
-	type Source struct {
-		client_id string
-		link      string
-		name      string
-	}
-	type User struct {
-	}
-	type Location struct {
-		latitude  float32
-		longitude float32
-	}
-	type Annotation struct {
-		atype string // change later
-		value Location
-	}
-
-	type Post struct {
-		canonical_url string
-		created_at    string
-		entities      Entity
-		html          string
-		id            string
-		machine_only  bool
-		num_replies   int
-		num_reposts   int
-		num_stars     int
-		source        Source
-		text          string
-		thread_id     string
-		user          User
-		you_reposted  bool
-		you_starred   bool
-		annotations   []Annotation
-		reposters     []User
-		starred_by    []User
+		Meta Meta
+		Data []Post
 	}
 
 	adn_global := "https://api.app.net/posts/stream/global"
@@ -88,16 +117,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//global, err := ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
-	dec := json.NewDecoder(io.Reader(res.Body))
-	for {
-		var r Response
-		if err := dec.Decode(&r); err == io.EOF {
-			break
-		} else if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%+v\n", r)
+	body, err := ioutil.ReadAll(res.Body)
+	var r Response
+	err = json.Unmarshal(body, &r)
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Printf("%+v\n", r)
 }
