f8ecca2590ecde5ace414efe94dddc27a0499f17
Mark Fernandes <0xmf@outlook.com>
2015-02-17 05:01:51 -0500

    all ADN types now annotated with `json:`

--- src/fetch.go
+++ src/fetch.go
@@ -10,105 +10,105 @@ import (
 
 func main() {
 	type HashTag struct {
-		Len  int `json:"len"`
-		Name string
-		Pos  int
+		Len  int    `json:"len"`
+		Name string `json:"name"`
+		Pos  int    `json:"pos"`
 	}
 	type Link struct {
-		Url  string
-		Text string
-		Pos  int
-		Len  int `json:"len"`
+		Url  string `json:"url"`
+		Text string `json:"text"`
+		Pos  int    `json:"pos"`
+		Len  int    `json:"len"`
 	}
 	type Mention struct{}
 	type Entity struct {
-		Mentions []Mention
-		Hashtags []HashTag
-		Links    []Link
+		Mentions []Mention `json:"mentions"`
+		Hashtags []HashTag `json:"hashtags"`
+		Links    []Link    `json:"links"`
 	}
 	type Source struct {
-		Link      string
-		Name      string
-		Client_Id string
+		Link      string `json:"link"`
+		Name      string `json:"name"`
+		Client_Id string `json:"client_id"`
 	}
 	type Image struct {
-		Url        string
-		Width      int
-		Is_Defualt bool
-		Height     int
+		Url        string `json:"url"`
+		Width      int    `json:"width"`
+		Is_Defualt bool   `json:"is_default"`
+		Height     int    `json:"height"`
 	}
 	type Description struct {
-		Text     string
-		Html     string
-		Entities Entity
+		Text     string `json:"text"`
+		Html     string `json:"html"`
+		Entities Entity `json:"entity"`
 	}
-	type Count struct {
-		Following int
-		Posts     int
-		Stars     int
+	type Counts struct {
+		Following int `json:"following"`
+		Posts     int `json:"posts"`
+		Stars     int `json:"stars"`
 	}
 	type User struct {
 		UserName      string      `json:"username"`
 		Avatar        Image       `json:"avatar"`
-		Profile       Description `json:"description"`
-		Locale        string
-		Created_At    string
-		Canonical_Url string
-		Cover         Image `json:"cover_image"`
-		Timezone      string
-		Counts        Count
-		Type          string `json:"type"`
-		Id            string
-		FullName      string `json:"name"`
+		Description   Description `json:"description"`
+		Locale        string      `json:"locale"`
+		Created_At    string      `json:"created_at"`
+		Canonical_Url string      `json:"canonical_url"`
+		Cover         Image       `json:"cover_image"`
+		Timezone      string      `json:"timezone"`
+		Counts        Counts      `json:"counts"`
+		Type          string      `json:"type"`
+		Id            string      `json:"id"`
+		Name          string      `json:"name"`
 	}
-	type Location struct {
-		Latitude  float32
-		Longitude float32
+	type Value struct {
+		Latitude  float32 `json:"latitude"`
+		Longitude float32 `json:"longitude"`
 	}
 	type Annotation struct {
-		Annotation_type string `json:"type"`
-		Value           Location
+		Type  string `json:"type"`
+		Value Value  `json:"value"`
 	}
 
 	type Post struct {
-		Created_At    string
-		Num_Stars     int
-		Num_Replies   int
-		Source        Source
-		Text          string
-		Num_Reposts   int
-		Id            string
-		Canonical_Url string
-		Entities      Entity
-		Html          string
-		Machine_Only  bool
-		Post_User     User `json:"user"`
-		Thread_Id     string
-		Pagination_Id string
-		You_Reposted  bool
-		You_Starred   bool
-		Annotations   []Annotation
-		Reposters     []User
-		Starred_By    []User
+		Created_At    string       `json:"created_at"`
+		Num_Stars     int          `json:"num_stars"`
+		Num_Replies   int          `json:"num_replies"`
+		Source        Source       `json:"source"`
+		Text          string       `json:"text"`
+		Num_Reposts   int          `json:"num_reposts"`
+		Id            string       `json:"id"`
+		Canonical_Url string       `json:"canonical_url"`
+		Entities      Entity       `json:"entities"`
+		Html          string       `json:"html"`
+		Machine_Only  bool         `json:"machine_only"`
+		User          User         `json:"user"`
+		Thread_Id     string       `json:"thread_id"`
+		Pagination_Id string       `json:"pagination_id"`
+		You_Reposted  bool         `json:"you_reposted"`
+		You_Starred   bool         `json:"you_starred"`
+		Annotations   []Annotation `json:"annotations"`
+		Reposters     []User       `json:"reposters"`
+		Starred_By    []User       `json:"starred_by"`
 	}
 
 	type Marker struct {
-		Id         string
-		Name       string
-		Percentage int
-		Updated_at string
-		Version    string
+		Id         string `json:"id"`
+		Name       string `json:"name"`
+		Percentage int    `json:"percentage"`
+		Updated_At string `json:"updated_at"`
+		Version    string `json:"version"`
 	}
 	type Meta struct {
-		Min_Id      string `json:"min_id"`
-		Code        int    `json:"code"`
-		Meta_Marker Marker
-		Max_Id      string `json:"max_id"`
-		More        bool   `json:"more"`
+		Min_Id string `json:"min_id"`
+		Code   int    `json:"code"`
+		Marker Marker `json:"marker"`
+		Max_Id string `json:"max_id"`
+		More   bool   `json:"more"`
 	}
 	type Response struct {
-		Meta Meta
-		Data []Post
+		Meta Meta   `json:"meta"`
+		Data []Post `json:"data"`
 	}
 
 	adn_global := "https://api.app.net/posts/stream/global"
@@ -125,6 +125,6 @@ func main() {
 		log.Fatal(err)
 	}
 	for _, p := range r.Data {
-		fmt.Printf("%s: %s\n", p.Post_User.UserName, p.Text)
+		fmt.Printf("%s: %s\n", p.User.UserName, p.Text)
 	}
 }
