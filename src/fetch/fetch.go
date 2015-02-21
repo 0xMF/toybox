dd7f6a2ab2c2a44095edd9e8642431c93148ea74
Mark Fernandes <0xmf@outlook.com>
2015-02-20 23:24:28 -0500

    added boltdb support (broken on Win/U VBox share)

--- src/fetch/fetch.go
+++ src/fetch/fetch.go
@@ -4,17 +4,42 @@
 package main
 
 import (
-	"fetch/adn"
+	//	"fetch/adn"
 	"fmt"
+	"github.com/boltdb/bolt"
 	"log"
 )
 
 func main() {
-	r, err := adn.GetGlobal()
+
+	db, err := bolt.Open("blog.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, p := range r.Data {
-		fmt.Printf("%s: %s\n", p.User.UserName, p.Text)
-	}
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("posts"))
+		if err != nil {
+			return err
+		}
+		return b.Put([]byte("0xMF"), []byte("Hey, it worked!"))
+	})
+
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("posts"))
+		v := b.Get([]byte("0xMF"))
+		fmt.Printf("that was: %s\n", v)
+		return nil
+	})
+
+	/*
+		r, err := adn.GetGlobal()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, p := range r.Data {
+			fmt.Printf("%s: %s\n", p.User.UserName, p.Text)
+		}
+	*/
 }
